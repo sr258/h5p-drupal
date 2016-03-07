8ed5b8faaa5bf5bf33546f8ccd528cb40e7611d4
Thomas Marstrander <thomasmars87@gmail.com>
2016-03-07 15:52:47 +0100

    Added new image editing tool to image widget. HFJ-1645

--- modules/h5peditor/h5peditor/h5peditor-file.class.php
+++ modules/h5peditor/h5peditor/h5peditor-file.class.php
@@ -34,22 +34,41 @@ class H5peditorFile {
     // Get the field.
     $this->field = json_decode($field);
 
-    if (function_exists('finfo_file')) {
-      $finfo = finfo_open(FILEINFO_MIME_TYPE);
-      $this->type = finfo_file($finfo, $_FILES['file']['tmp_name']);
-      finfo_close($finfo);
-    }
-    elseif (function_exists('mime_content_type')) {
-      // Deprecated, only when finfo isn't available.
-      $this->type = mime_content_type($_FILES['file']['tmp_name']);
+    // Check if uploaded base64 encoded file
+    if (isset($_POST) && isset($_POST['dataURI']) && $_POST['dataURI'] !== '') {
+      $data = $_POST['dataURI'];
+
+      // Extract data from string
+      list($type, $data) = explode(';', $data);
+      list(, $data)      = explode(',', $data);
+      $this->data = base64_decode($data);
+
+      // Extract file type and extension
+      list(, $type) = explode(':', $type);
+      list(, $extension) = explode('/', $type);
+      $this->type = $type;
+      $this->extension = $extension;
+      $this->size = strlen($this->data);
     }
     else {
-      $this->type = $_FILES['file']['type'];
+
+      // Handle temporarily uploaded form file
+      if (function_exists('finfo_file')) {
+        $finfo = finfo_open(FILEINFO_MIME_TYPE);
+        $this->type = finfo_file($finfo, $_FILES['file']['tmp_name']);
+        finfo_close($finfo);
+      }
+      elseif (function_exists('mime_content_type')) {
+        // Deprecated, only when finfo isn't available.
+        $this->type = mime_content_type($_FILES['file']['tmp_name']);
+      }
+      else {
+        $this->type = $_FILES['file']['type'];
+      }
+
+      $this->extension = pathinfo($_FILES['file']['name'], PATHINFO_EXTENSION);
+      $this->size = $_FILES['file']['size'];
     }
-
-    $this->extension = pathinfo($_FILES['file']['name'], PATHINFO_EXTENSION);
-
-    $this->size = $_FILES['file']['size'];
   }
 
   /**
@@ -117,7 +136,15 @@ class H5peditorFile {
           return FALSE;
         }
 
-        $image = @getimagesize($_FILES['file']['tmp_name']);
+        // Get image size from base64 string
+        if (isset($this->data)) {
+          $image = getimagesizefromstring($this->data);
+        }
+        else {
+          // Image size from temp file
+          $image = @getimagesize($_FILES['file']['tmp_name']);
+        }
+
         if (!$image) {
           $this->result->error = $this->interface->t('File is not an image.');
           return FALSE;
@@ -170,20 +197,33 @@ class H5peditorFile {
     return TRUE;
   }
 
- public function copy() {
+  public function copy() {
     $matches = array();
     preg_match('/([a-z0-9]{1,})$/i', $_FILES['file']['name'], $matches);
 
     $this->name = uniqid($this->field->name . '-');
-    if (isset($matches[0])) {
+
+    // Add extension to name
+    if (isset($this->data)) {
+      $this->name .= '.' . $this->extension;
+    }
+    else if (isset($matches[0])) {
       $this->name .= '.' . $matches[0];
     }
-    $this->name = $this->field->type . 's/' . $this->name;
 
+    $this->name = $this->field->type . 's/' . $this->name;
     $this->path = $this->files_directory . '/' . $this->name;
-    if (!copy($_FILES['file']['tmp_name'], $this->path)) {
-      $this->result->error = $this->interface->t('Could not copy file.');
-      return FALSE;
+
+    // Save file to path
+    if (isset($this->data)) {
+      file_put_contents($this->path, $this->data);
+    }
+    else {
+      // Copy tmp file to new path
+      if (!copy($_FILES['file']['tmp_name'], $this->path)) {
+        $this->result->error = $this->interface->t('Could not copy file.');
+        return FALSE;
+      }
     }
 
     $this->result->path = $this->name;
---
+++ modules/h5peditor/h5peditor/scripts/h5peditor-image-popup.js
@@ -0,0 +1,283 @@
+var H5PEditor = H5PEditor || {};
+
+H5PEditor.ImageEditingPopup = (function ($, EventDispatcher) {
+  var instanceCounter = 0;
+  var scriptsLoaded = false;
+  var editorPath = '../../sites/all/modules/h5p/modules/h5peditor/h5peditor/';
+
+  /**
+   * Popup for editing images
+   *
+   * @param {number} [ratio] Ratio that cropping must keep
+   * @constructor
+   */
+  function ImageEditingPopup(ratio) {
+    EventDispatcher.call(this);
+    var self = this;
+    var uniqueId = instanceCounter;
+    var isShowing = false;
+    var isReset = false;
+    var topOffset = 0;
+    var maxWidth;
+    var maxHeight;
+
+    /**
+     * Create header button
+     *
+     * @param {string} coreString Must be specified in core translations
+     * @param {string} className Unique button identifier that will be added to classname
+     * @param {function} clickEvent OnClick function
+     */
+    var createButton = function (coreString, className, clickEvent) {
+      var button = document.createElement('button');
+      button.textContent = ns.t('core', coreString);
+      button.className = className;
+      button.onclick = clickEvent;
+      headerButtons.appendChild(button);
+    };
+
+    /**
+     * Set max width and height for image editing tool
+     */
+    var setDarkroomDimensions = function () {
+      // Set max dimensions
+      var style = getComputedStyle(popup);
+      maxWidth = popup.offsetWidth - parseInt(style.paddingLeft) - parseInt(style.paddingRight);
+
+      // Only use 65% of screen height
+      var maxScreenHeight = screen.height * 0.65;
+      var editorHeight = background.offsetHeight;
+
+      // Use smallest of screen height and editor height,
+      // we don't want to overflow editor or screen
+      maxHeight = maxScreenHeight < editorHeight ? maxScreenHeight : editorHeight;
+    };
+
+    /**
+     * Load scripts dynamically
+     */
+    var loadScripts = function () {
+      loadScript(editorPath + 'libs/fabric.js', function () {
+        loadScript(editorPath + 'libs/darkroom.js', function () {
+          createDarkroom();
+          scriptsLoaded = true;
+        });
+      });
+    };
+
+    /**
+     * Load a script dynamically
+     *
+     * @param {string} path Path to script
+     * @param {function} [callback]
+     */
+    var loadScript = function (path, callback) {
+      $.ajax({
+        url: path,
+        dataType: 'script',
+        success: function () {
+          if (callback) {
+            callback();
+          }
+        },
+        async: true
+      });
+    };
+
+    /**
+     * Grab canvas data and pass data to listeners.
+     */
+    var saveImage = function() {
+
+      // Check if image has changed
+      if (self.darkroom.transformations.length || isReset) {
+        // Remove current cropping region
+        self.darkroom.plugins.crop.releaseFocus();
+
+        var newImage = self.darkroom.canvas.toDataURL();
+        self.trigger('savedImage', newImage);
+      }
+
+      isReset = false;
+    };
+
+    /**
+     * Create image editing tool from image.
+     */
+    var createDarkroom = function () {
+      requestAnimationFrame(function () {
+        setDarkroomDimensions();
+
+        self.darkroom = new Darkroom('#h5p-editing-image-' + uniqueId, {
+          initialize: function () {
+            self.adjustPopupOffset();
+            imageLoading.classList.add('hidden');
+          },
+          maxWidth: maxWidth,
+          maxHeight: maxHeight,
+          plugins: {
+            crop: {
+              ratio: ratio || null
+            },
+            save : false
+          }
+        });
+      });
+    };
+
+    /**
+     * Adjust popup offset.
+     * Make sure it is centered on top of offset.
+     *
+     * @param {Object} [offset] Offset that popup should center on.
+     * @param {number} [offset.top] Offset to top.
+     */
+    this.adjustPopupOffset = function (offset) {
+      if (offset) {
+        topOffset = offset.top;
+      }
+
+      var offsetCentered = topOffset - (popup.offsetHeight / 2);
+
+      // Min offset is 0
+      offsetCentered = offsetCentered > 0 ? offsetCentered : 0;
+      popup.style.top = offsetCentered + 'px';
+    };
+
+    /**
+     * Set new image in editing tool
+     *
+     * @param {string} imgSrc Source of new image
+     */
+    this.setImage = function (imgSrc) {
+      // Set new image
+      var darkroom = popup.querySelector('.darkroom-container');
+      if (darkroom) {
+        darkroom.parentNode.removeChild(darkroom);
+      }
+
+      editingImage.src = imgSrc;
+      imageLoading.classList.remove('hidden');
+      editingImage.classList.add('hidden');
+      editingContainer.appendChild(editingImage);
+
+      createDarkroom();
+    };
+
+    /**
+     * Show popup
+     *
+     * @param {Object} [offset] Offset that popup should center on.
+     * @param {string} [imageSrc] Source of image that will be edited
+     */
+    this.show = function (offset, imageSrc) {
+      if (imageSrc) {
+
+        // Load image editing scripts dynamically
+        if (!scriptsLoaded) {
+          editingImage.src = imageSrc;
+          loadScripts();
+        }
+        else {
+          self.setImage(imageSrc);
+        }
+      }
+
+      isShowing = true;
+      background.classList.remove('hidden');
+      this.adjustPopupOffset(offset);
+    };
+
+    /**
+     * Hide popup
+     */
+    this.hide = function () {
+      isShowing = false;
+      background.classList.add('hidden');
+    };
+
+    /**
+     * Toggle popup visibility
+     */
+    this.toggle = function () {
+      if (isShowing) {
+        this.hide();
+      }
+      else {
+        this.show();
+      }
+    };
+
+    // Create elements
+    var background = document.createElement('div');
+    background.className = 'h5p-editing-image-popup-background hidden';
+
+    var popup = document.createElement('div');
+    popup.className = 'h5p-editing-image-popup';
+    background.appendChild(popup);
+
+    var header = document.createElement('div');
+    header.className = 'h5p-editing-image-header';
+    popup.appendChild(header);
+
+    var headerTitle = document.createElement('div');
+    headerTitle.className = 'h5p-editing-image-header-title';
+    headerTitle.textContent = 'Edit Image!';
+    header.appendChild(headerTitle);
+
+    var headerButtons = document.createElement('div');
+    headerButtons.className = 'h5p-editing-image-header-buttons';
+    header.appendChild(headerButtons);
+
+    // Create header buttons
+    createButton('resetToOriginalLabel', 'h5p-editing-image-reset-button h5p-remove', function () {
+      self.trigger('resetImage');
+      isReset = true;
+    });
+    createButton('cancelLabel', 'h5p-editing-image-cancel-button', function () {
+      self.trigger('canceled');
+      self.hide();
+    });
+    createButton('saveLabel', 'h5p-editing-image-save-button h5p-done', function () {
+      saveImage();
+      self.hide();
+    });
+
+    var editingContainer = document.createElement('div');
+    editingContainer.className = 'h5p-editing-image-editing-container';
+    popup.appendChild(editingContainer);
+
+    var imageLoading = document.createElement('div');
+    imageLoading.className = 'h5p-editing-image-loading';
+    imageLoading.textContent = ns.t('core', 'loadingImageEditor');
+    popup.appendChild(imageLoading);
+
+    // Create editing image
+    var editingImage = new Image();
+    editingImage.className = 'h5p-editing-image hidden';
+    editingImage.id = 'h5p-editing-image-' + uniqueId;
+    editingContainer.appendChild(editingImage);
+
+    // Append to body
+    H5P.$body.get(0).appendChild(background);
+
+    // Close popup on background click
+    background.onclick = function () {
+      this.hide();
+    }.bind(this);
+
+    // Prevent closing popup
+    popup.onclick = function (e) {
+      e.stopPropagation();
+    };
+
+    // Make sure each ImageEditingPopup instance has a unique ID
+    instanceCounter++;
+  }
+
+  ImageEditingPopup.prototype = Object.create(EventDispatcher.prototype);
+  ImageEditingPopup.prototype.constructor = ImageEditingPopup;
+
+  return ImageEditingPopup;
+
+})(H5P.jQuery, H5P.EventDispatcher);
