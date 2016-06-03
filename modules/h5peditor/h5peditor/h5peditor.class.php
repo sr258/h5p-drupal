83a4d78390d36bd6d0226e2823f79ed8e46a4126
Frode Petterson <frode.andre.petterson@gmail.com>
2016-06-03 13:08:01 +0200

    Add extra ajax params through URL
    Fixes compability with old versions of IV and Collage editors.
    Updated core + editor libraries
    HFJ-1983

--- modules/h5peditor/h5peditor/h5peditor.class.php
+++ modules/h5peditor/h5peditor/h5peditor.class.php
@@ -4,9 +4,7 @@ class H5peditor {
 
   public static $styles = array(
     'libs/darkroom.css',
-    'styles/css/application.css',
-    'styles/css/h5peditor-image.css',
-    'styles/css/h5peditor-image-popup.css'
+    'styles/css/application.css'
   );
   public static $scripts = array(
     'scripts/h5peditor.js',
@@ -18,6 +16,7 @@ class H5peditor {
     'scripts/h5peditor-html.js',
     'scripts/h5peditor-number.js',
     'scripts/h5peditor-textarea.js',
+    'scripts/h5peditor-file-uploader.js',
     'scripts/h5peditor-file.js',
     'scripts/h5peditor-image.js',
     'scripts/h5peditor-image-popup.js',
